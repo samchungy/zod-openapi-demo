0da40b88329edde99e274e5a7757276eda54eb6d
Sam Chung <samc@seek.com.au>
2023-02-21 14:57:01 +1100

    push demo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
   extends: ['skuba'],
-  // plugins: ['eslint-plugin-zod-to-openapi'],
-  // overrides: [
-  //   {
-  //     files: ['src/types/jobs.ts'],
-  //     rules: {
-  //       'zod-to-openapi/require-openapi': 'error',
-  //       'zod-to-openapi/require-comment': 'error',
-  //       'zod-to-openapi/require-example': 'error',
-  //       'zod-to-openapi/prefer-openapi-last': 'error',
-  //       'zod-to-openapi/prefer-zod-default': 'error',
-  //     },
-  //   },
-  // ],
+  plugins: ['eslint-plugin-zod-to-openapi'],
+  overrides: [
+    {
+      files: ['src/types/jobs.ts'],
+      rules: {
+        'zod-to-openapi/require-openapi': 'error',
+        'zod-to-openapi/require-comment': 'error',
+        'zod-to-openapi/require-example': 'error',
+        'zod-to-openapi/prefer-openapi-last': 'error',
+        'zod-to-openapi/prefer-zod-default': 'error',
+      },
+    },
+  ],
 };
--- src/types/jobs.ts
+++ src/types/jobs.ts
@@ -1,4 +1,8 @@
+import { extendZodWithOpenApi } from '@asteasolutions/zod-to-openapi';
 import { z } from 'zod';
+
+import { registry } from 'src/schema/registry';
+extendZodWithOpenApi(z);
 
 export interface Job {
   id: string;
@@ -10,17 +14,69 @@ export interface Job {
 
 export type JobInput = z.infer<typeof JobInputSchema>;
 
+/**
+ * Hirer Details
+ */
+const HirerDetailsSchema = registry.register(
+  'hirerDetails',
+  z
+    .object({
+      /**
+       * SEEK ID
+       * @example "60002023"
+       */
+      id: z.string().openapi({ description: 'SEEK ID', example: '60002023' }),
+    })
+    .openapi({ description: 'Hirer Details', type: 'boolean' }),
+);
+
 export const JobInputSchema = z.object({
-  hirer: z.object({
-    id: z.string(),
+  /**
+   * Hirer Details
+   */
+  hirer: HirerDetailsSchema,
+  /**
+   * Name of job visible in job search
+   * @example "#typescriptification host"
+   */
+  title: z.string().max(100).openapi({
+    description: 'Name of job visible in job search',
+    example: '#typescriptification host',
   }),
+  /**
+   * Metadata
+   */
+  metadata: z
+    .object({
+      /**
+       * @deprecated Performance of the ad 3
+       * @example "A"
+       */
+      performanceTier: z.enum(['A', 'B', 'C']).openapi({
+        description: 'Performance of the ad 3',
+        example: 'A',
+        deprecated: true,
+      }),
+    })
+    .openapi({ description: 'Metadata' }),
 });
+
+const job: JobInput = {
+  hirer: {
+    id: '1234',
+  },
+  title: 'host',
+  metadata: {
+    performanceTier: 'A',
+  },
+};
 
 export type JobOutput = z.infer<typeof JobOutputSchema>;
 
 export const JobOutputSchema = z.object({
   id: z.string().uuid(),
-  hirer: z.object({
-    id: z.string(),
-  }),
+  /**
+   * Hirer Details
+   */
+  hirer: HirerDetailsSchema,
 });
