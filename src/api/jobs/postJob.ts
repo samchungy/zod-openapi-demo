85dc1f85efd3b623648f07c84ed725b3e9308f85
samchungy <samchungy@gmail.com>
2023-07-10 12:14:11 +1000

    fix output type

--- src/api/jobs/postJob.ts
+++ src/api/jobs/postJob.ts
@@ -2,13 +2,13 @@ import { logger } from 'src/framework/logging';
 import { metricsClient } from 'src/framework/metrics';
 import { validateRequestBody } from 'src/framework/validation';
 import * as storage from 'src/storage/jobs';
-import { JobInputSchema } from 'src/types/jobs';
+import { JobInputSchema, JobOutput } from 'src/types/jobs';
 import { Middleware } from 'src/types/koa';
 
 export const postJobHandler: Middleware = async (ctx) => {
   const jobInput = validateRequestBody(ctx, JobInputSchema);
 
-  const job = await storage.createJob(jobInput);
+  const job: JobOutput = await storage.createJob(jobInput);
 
   // no PII in these jobs
   logger.debug({ job }, 'created job');
